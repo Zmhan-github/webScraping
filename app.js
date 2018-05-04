29ffc3dbca06e2fb727af6038b98178dcd67d043
Marat Zh <regeon-93@inbox.ru>
2018-05-05 03:10:54 +0500

    add dashboard ui

--- app.js
+++ app.js
@@ -11,21 +11,40 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 var ParserApp = function (_React$Component) {
   _inherits(ParserApp, _React$Component);
 
-  function ParserApp() {
+  function ParserApp(props) {
     _classCallCheck(this, ParserApp);
 
-    return _possibleConstructorReturn(this, (ParserApp.__proto__ || Object.getPrototypeOf(ParserApp)).apply(this, arguments));
+    var _this = _possibleConstructorReturn(this, (ParserApp.__proto__ || Object.getPrototypeOf(ParserApp)).call(this, props));
+
+    _this.state = {
+      DashboardVisible: false
+    };
+
+    _this.showDashboard = _this.showDashboard.bind(_this);
+    _this.hideDashboard = _this.hideDashboard.bind(_this);
+    return _this;
   }
 
   _createClass(ParserApp, [{
+    key: "showDashboard",
+    value: function showDashboard() {
+      this.setState({ DashboardVisible: true });
+    }
+  }, {
+    key: "hideDashboard",
+    value: function hideDashboard() {
+      this.setState({ DashboardVisible: false });
+    }
+  }, {
     key: "render",
     value: function render() {
+      var DashboardVisible = this.state.DashboardVisible;
+
       return React.createElement(
         "div",
         { className: "container" },
-        React.createElement(Login, null),
-        null && React.createElement(Steps, null),
-        null && React.createElement(Result, null)
+        DashboardVisible || React.createElement(Login, { showDashboard: this.showDashboard }),
+        DashboardVisible && React.createElement(Dashboard, { hideDashboard: this.hideDashboard })
       );
     }
   }]);
@@ -36,27 +55,31 @@ var ParserApp = function (_React$Component) {
 var Login = function (_React$Component2) {
   _inherits(Login, _React$Component2);
 
-  function Login() {
+  function Login(props) {
     _classCallCheck(this, Login);
 
-    return _possibleConstructorReturn(this, (Login.__proto__ || Object.getPrototypeOf(Login)).apply(this, arguments));
+    var _this2 = _possibleConstructorReturn(this, (Login.__proto__ || Object.getPrototypeOf(Login)).call(this, props));
+
+    _this2.state = {
+      login: "test",
+      password: "12345"
+    };
+
+    _this2.submitForm = _this2.submitForm.bind(_this2);
+    return _this2;
   }
 
   _createClass(Login, [{
     key: "submitForm",
     value: function submitForm(e) {
       e.preventDefault();
-
-      var defaultClientAuth = {
-        login: "test",
-        password: "12345"
-      };
-
       var login = e.target.elements.login.value.trim();
       var password = e.target.elements.password.value.trim();
 
-      if (defaultClientAuth.login === login && defaultClientAuth.password === password) {
-        alert("login: " + login + "\npassword: " + password);
+      if (this.state.login === login && this.state.password === password) {
+        console.log("login: " + login + "\npassword: " + password);
+        console.log("Actions:", this.props.showDashboard);
+        this.props.showDashboard();
       }
     }
   }, {
@@ -94,29 +117,77 @@ var Login = function (_React$Component2) {
   return Login;
 }(React.Component);
 
-var Steps = function (_React$Component3) {
-  _inherits(Steps, _React$Component3);
+var Dashboard = function (_React$Component3) {
+  _inherits(Dashboard, _React$Component3);
 
-  function Steps() {
-    _classCallCheck(this, Steps);
+  function Dashboard(props) {
+    _classCallCheck(this, Dashboard);
 
-    return _possibleConstructorReturn(this, (Steps.__proto__ || Object.getPrototypeOf(Steps)).apply(this, arguments));
+    var _this3 = _possibleConstructorReturn(this, (Dashboard.__proto__ || Object.getPrototypeOf(Dashboard)).call(this, props));
+
+    _this3.state = {
+      site: "www.squidtv.net",
+      path: "india"
+    };
+    console.log(_this3);
+    return _this3;
   }
 
-  _createClass(Steps, [{
+  _createClass(Dashboard, [{
     key: "render",
     value: function render() {
+      var hideDashboard = this.props.hideDashboard;
+      var _state = this.state,
+          site = _state.site,
+          path = _state.path;
+
       return React.createElement(
-        "main",
-        null,
-        React.createElement(Step, null),
-        React.createElement(Step, null),
-        React.createElement(Step, null)
+        "div",
+        { className: "dashboard-box" },
+        React.createElement(
+          "header",
+          null,
+          React.createElement(
+            "nav",
+            null,
+            React.createElement(
+              "ul",
+              null,
+              React.createElement(
+                "li",
+                null,
+                "WeB Site:"
+              ),
+              React.createElement(
+                "li",
+                null,
+                site,
+                "/",
+                path
+              )
+            )
+          ),
+          React.createElement(
+            "button",
+            { onClick: function onClick() {
+                return hideDashboard();
+              } },
+            "\u0412\u044B\u0445\u043E\u0434"
+          )
+        ),
+        React.createElement(
+          "main",
+          null,
+          React.createElement(Step, null),
+          React.createElement(Step, null),
+          React.createElement(Step, null)
+        ),
+        React.createElement(Result, null)
       );
     }
   }]);
 
-  return Steps;
+  return Dashboard;
 }(React.Component);
 
 var Step = function (_React$Component4) {
@@ -132,9 +203,28 @@ var Step = function (_React$Component4) {
     key: "render",
     value: function render() {
       return React.createElement(
-        "div",
-        null,
-        "Steps"
+        "select",
+        { defaultValue: 1 },
+        React.createElement(
+          "option",
+          { value: "0" },
+          "\u0412\u044B\u0431\u0435\u0440\u0438\u0442\u0435 \u0433\u043E\u0440\u043E\u0434 "
+        ),
+        React.createElement(
+          "option",
+          { value: "1" },
+          "\u041C\u043E\u0441\u043A\u0432\u0430 "
+        ),
+        React.createElement(
+          "option",
+          { value: "2" },
+          "\u0421\u0430\u043D\u043A\u0442-\u041F\u0435\u0442\u0435\u0440\u0431\u0443\u0440\u0433 "
+        ),
+        React.createElement(
+          "option",
+          { value: "3" },
+          "\u0411\u0435\u0440\u043B\u0438\u043D"
+        )
       );
     }
   }]);
@@ -155,7 +245,7 @@ var Result = function (_React$Component5) {
     key: "render",
     value: function render() {
       return React.createElement(
-        "div",
+        "footer",
         null,
         "Result"
       );
